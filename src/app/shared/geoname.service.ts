79c947183812def95b0e993e2c2a458d24c17041
jeanecard <jean.ecard@outlook.fr>
2020-04-19 16:27:00 +0200

    deploy

--- src/app/shared/geoname.service.ts
+++ src/app/shared/geoname.service.ts
@@ -9,7 +9,7 @@ import { delay } from 'rxjs/operators';
 })
 export class GeonameService {
 
-  private _geonameServiceURLStart = 'https://hrcorebordersservicesv-3-1.azurewebsites.net/api/HRGeoname/'
+  private _geonameServiceURLStart = 'https://hrcorebordersservicesv-3-1.azurewebsites.net/api/v1.0/HRGeoname/HRGeoname/'
   private _passage = 1;
 
   constructor(private http: HttpClient) { }
