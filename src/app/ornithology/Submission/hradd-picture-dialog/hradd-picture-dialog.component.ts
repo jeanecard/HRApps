cd67fe24839c1dc1d547987a2ebcf6ff0cc0735d
jeanecard <jean.ecard@outlook.fr>
2021-03-13 18:54:41 +0100

    wip

--- src/app/ornithology/Submission/hradd-picture-dialog/hradd-picture-dialog.component.ts
+++ src/app/ornithology/Submission/hradd-picture-dialog/hradd-picture-dialog.component.ts
@@ -9,7 +9,6 @@ import { HrSubmitSource } from 'src/app/model/Ornitho/hr-submit-source';
 import { HrSubmitGender } from 'src/app/model/Ornitho/hr-submit-gender';
 import { HrSubmitAge } from 'src/app/model/Ornitho/hr-submit-age';
 
-
 const UNASSIGNED_VALUE_DISPLAY = " _ ";
 const MORE_INFO_DISPLAY = " ...";
 @Component({
@@ -30,7 +29,9 @@ export class HRAddPictureDialogComponent implements OnInit {
   public sources: Observable<HrSubmitSource[]>;
   public genders: Observable<HrSubmitGender[]>;
   public ageTypes: Observable<HrSubmitAge[]>;
-  url: string | ArrayBuffer;
+  public files: any[] = [];
+  private MAX_SIZE = 20971520;
+  private ALLOWED_TYPES = ['image/png', 'image/jpeg'];
   cardImageBase64: string;
   imageError: string;
   isUploadingState = false;
@@ -38,7 +39,7 @@ export class HRAddPictureDialogComponent implements OnInit {
 
   isLinear = false;
 
-  theFile: any = null;
+  // theFile: any = null;
   messages: string[] = [];
 
 
@@ -70,8 +71,15 @@ export class HRAddPictureDialogComponent implements OnInit {
     this.ageTypes = this._picService.getAges();
   }
 
+  /**
+  *  Save data :
+  * 1- Save Metadata
+  * 2- Save corresponding uploaded file
+  */
   public onYesClick(): void {
+
     this.updateModelFromView();
+    //1- Upload metadata
     if (this.data?.id) {
       this._picService.updateImage(this._model).subscribe({
         next:
@@ -88,7 +96,7 @@ export class HRAddPictureDialogComponent implements OnInit {
     } else {
       this._picService.addImageData(this._model).subscribe({
         next:
-          data => { 
+          data => {
             this.dialogRef.close(data);
           },
         error: (dataError) => {
@@ -98,12 +106,20 @@ export class HRAddPictureDialogComponent implements OnInit {
         }
       });
     }
+    //2- Upload file
+    this.readAndUploadFile();
   }
 
+  /**
+  * Close dialog.
+  */
   onNoClick(): void {
     this.dialogRef.close();
   }
 
+  /**
+*  update this._model from view data
+*/
   private updateModelFromView(): void {
     this._model.credit = this.credit.value;
     this._model.id = this.data.id;
@@ -112,57 +128,43 @@ export class HRAddPictureDialogComponent implements OnInit {
     this._model.sourceType = this.source.value?.id;
     this._model.ageType = this.ageType.value?.id;
   }
-  // CODE DROP
-  files: any[] = [];
 
   /**
-   * on file drop handler
+   * on file dropped handler
    */
-  onFileDropped($event) {
+  public onFileDropped($event) {
     this.prepareFilesList($event);
-  }
-
-  /**
-   * handle file from browsing
-   */
-  fileBrowseHandler(files) {
-    this.prepareFilesList(files);
   }
 
   /**
    * Delete file from files list
    * @param index (File index)
    */
-  deleteFile(index: number) {
+  public deleteFile(index: number) {
     this.files.splice(index, 1);
   }
 
-
-
   /**
-   * Convert Files list to normal array list
+   * Convert Files list to normal array list of a single item.
    * @param files (Files List)
    */
-  prepareFilesList(files: Array<any>) {
-    for (const item of files) {
+  prepareFilesList(inputfiles: Array<any>) {
+    // 1- Clear existing file list.
+    this.files = [];
+    for (const item of inputfiles) {
       item.progress = 0;
       this.files.push(item);
-    }
-
-    const reader = new FileReader();
-    let img = new Image();
-    reader.readAsDataURL(files[0]);
-    reader.onload = (_event) => {
-      this.url = reader.result;
+      // 2- Push only first file selected.
+      break;
     }
   }
 
   /**
-   * format bytes
+   * format bytes for displaying in view.
    * @param bytes (File size in bytes)
    * @param decimals (Decimals point)
    */
-  formatBytes(bytes, decimals) {
+  public formatBytesForView(bytes, decimals) {
     if (bytes === 0) {
       return '0 Bytes';
     }
@@ -173,103 +175,90 @@ export class HRAddPictureDialogComponent implements OnInit {
     return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
   }
 
-
-
-
-
-
-  fileChangeEvent(fileInput: any) {
-    if (fileInput.target.files && fileInput.target.files[0]) {
-      // Size Filter Bytes
-      const max_size = 20971520;
-      const allowed_types = ['image/png', 'image/jpeg'];
-      const max_height = 15200;
-      const max_width = 25600;
-
-      if (fileInput.target.files[0].size > max_size) {
-        this.imageError =
-          'Maximum size allowed is ' + max_size / 1000 + 'Mb';
-
-        return false;
-      }
-
-      if (!_.includes(allowed_types, fileInput.target.files[0].type)) {
-        this.imageError = 'Only Images are allowed ( JPG | PNG )';
-        return false;
-      }
-
-
-      this.theFile = fileInput.target.files[0];
-      this.readAndUploadFile(this.theFile);
-
-      // a priori dummy
-      const reader = new FileReader();
-      reader.onload = (e: any) => {
-        const image = new Image();
-        image.src = e.target.result;
-        image.onload = rs => {
-          const img_height = rs.currentTarget['height'];
-          const img_width = rs.currentTarget['width'];
-
-          console.log(img_height, img_width);
-
-
-          if (img_height > max_height && img_width > max_width) {
-            this.imageError =
-              'Maximum dimentions allowed ' +
-              max_height +
-              '*' +
-              max_width +
-              'px';
-            return false;
-          } else {
+  /**
+ * Triggered when file(s) are selected
+ * 1- If files is not empty list
+ *  1.1- If size and type of the first file is valid
+ *    1.1.1- Create FileReader and define onLoad trigger
+ *    1.1.2- Launch file loading
+ * 2- else, dummy
+ * @param filesInput array of selected files
+ */
+  public fileChangeEvent(filesInput: any) {
+    // 1-
+    if (filesInput?.target?.files
+      && filesInput.target.files[0]) {
+      let firstFile = filesInput.target.files[0];
+      // 1.1-
+      if (!this.isErrorWhileCheckingTypeAndSize(filesInput)) {
+        // this.theFile = filesInput.target.files[0];
+        // 1.1.1-
+        const reader = new FileReader();
+        reader.onload = (e: any) => {
+          const image = new Image();
+          image.src = e.target.result;
+          image.onload = rs => {
             const imgBase64Path = e.target.result;
             this.cardImageBase64 = imgBase64Path;
-            this.files.push(fileInput.target.files[0]);
-          }
+            this.files.push(firstFile);
+          };
         };
-      };
-
-      reader.readAsDataURL(fileInput.target.files[0]);
+        // 1.1.2-
+        reader.readAsDataURL(filesInput.target.files[0]);
+      }
     }
   }
 
+  /**
+   * check if file is jpeg or png and size < MAX_SIZE.
+   * if not, set imageError property value.
+   * @param a file array
+   */
+  private isErrorWhileCheckingTypeAndSize(filesInput: any): boolean {
+    if (filesInput?.target?.files
+      && filesInput.target.files[0]) {
+      if (filesInput.target.files[0].size > this.MAX_SIZE) {
+        this.imageError =
+          'Maximum size allowed is ' + this.MAX_SIZE / 1000 + 'Mb';
+        return true;
+      }
 
-  private readAndUploadFile(theFile: any) {
-    let file = new FileToUpload();
+      if (!_.includes(this.ALLOWED_TYPES, filesInput.target.files[0].type)) {
+        this.imageError = 'Only Images are allowed ( JPG | PNG )';
+        return true;
+      }
+      return false;
+    }
+    return false;
+  }
+  /**
+   * Prepare data for service from uploaded file.
+   * 
+   */
+  private readAndUploadFile() {
 
-    // Set File Information
-    file.fileName = theFile.name;
-    file.fileSize = theFile.size;
-    file.fileType = theFile.type;
-    file.lastModifiedTime = theFile.lastModified;
-    file.lastModifiedDate = theFile.lastModifiedDate;
-    file.submittedPicture = new HRPictureOrnithoAddInput();
-    file.submittedPicture.ageType = this.ageType.value?.id;
-    file.submittedPicture.genderType = this.gender.value?.id;
-    file.submittedPicture.sourceType = this.source.value?.id;
-    file.submittedPicture.vernacularName = this.data?.vernacularName;
-    file.submittedPicture.credit = this.credit.value;
+    let selectedFile = this.files[0];
+    if (selectedFile) {
+      let fileForService = new FileToUpload();
+      // Set File Information
+      fileForService.fileName = selectedFile.name;
+      fileForService.fileSize = selectedFile.size;
+      fileForService.fileType = selectedFile.type;
+      fileForService.lastModifiedTime = selectedFile.lastModified;
+      fileForService.lastModifiedDate = selectedFile.lastModifiedDate;
+      fileForService.submittedPicture = new HRPictureOrnithoAddInput();
+      fileForService.submittedPicture.ageType = this.ageType.value?.id;
+      fileForService.submittedPicture.genderType = this.gender.value?.id;
+      fileForService.submittedPicture.sourceType = this.source.value?.id;
+      fileForService.submittedPicture.vernacularName = this.data?.vernacularName;
+      fileForService.submittedPicture.credit = this.credit.value;
 
- 
-
-    // Use FileReader() object to get file to upload
-    // NOTE: FileReader only works with newer browsers
-    let reader = new FileReader();
-
-    // Setup onload event for reader
-    reader.onload = () => {
-      // Store base64 encoded representation of file
-      file.fileAsBase64 = reader.result.toString();
-
+      fileForService.fileAsBase64 = this.cardImageBase64;
       // POST to server
-      this._picService.uploadFile(file).subscribe(resp => {
+      this._picService.uploadFile(fileForService).subscribe(resp => {
         this.messages.push("Upload complete");
       });
     }
-
-    // Read the file
-    reader.readAsDataURL(theFile);
   }
 
   public getReducedText(value: string): string {
@@ -293,5 +282,4 @@ export class HRAddPictureDialogComponent implements OnInit {
       return UNASSIGNED_VALUE_DISPLAY;
     }
   }
-
 }
